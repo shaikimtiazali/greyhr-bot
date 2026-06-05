18d7e788dcae1ea0c3e41e9c803833c8719e7715
shaikimtiazali <imtiazali.ali36@gmail.com>
2026-06-05 11:23:40 +0530

    chore: remove Render/Docker files, keep GitHub Actions only

--- src/test-run.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require("child_process");
-
-console.log("Manual test run triggered...");
-execSync("node src/actions/run.js", {
-  stdio: "inherit",
-  env: { ...process.env, TZ: "Asia/Kolkata" },
-  timeout: 15 * 60 * 1000,
-});
